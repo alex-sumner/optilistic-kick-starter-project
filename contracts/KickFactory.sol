49e0078a4758f7efa0fe9bd6bd5e264e3999dab8
Alex Sumner <alex_sumner@mentor.com>
2021-08-16 17:39:13 +0100

    raised pragma minimum solidity version to 0.8.0

--- contracts/KickFactory.sol
+++ contracts/KickFactory.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity >=0.7.0 <0.9.0;
+pragma solidity >=0.8.0 <0.9.0;
 
 import {KickProject} from "./KickProject.sol";
 import "hardhat/console.sol";
